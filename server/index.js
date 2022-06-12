47fa29fab321f5fcba872a4a815d732e1cab328a
Bianca DeHaan <biancad88@yahoo.com>
2022-06-12 00:47:00 -0400

    fixing other

--- server/index.js
+++ server/index.js
@@ -5,6 +5,9 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../mtgordle-app/build')));
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
   });
