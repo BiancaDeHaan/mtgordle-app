243d943e7a44dd24e816e91299ab77d782d4c3af
Bianca DeHaan <biancad88@yahoo.com>
2022-06-17 00:12:17 -0400

    fixing midnight reset

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 // https://api.scryfall.com/cards/random?q=f%3Astandard
 
-var card_name = "Hive of the Eye Tyrant";
+var card_name = "Goblin Grenade";
 
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../mtgordle-app/build')));
